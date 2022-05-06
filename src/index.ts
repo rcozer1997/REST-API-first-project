a5c397736d36e4d98575f37f282d0f761b4ba28b
Rafael Cozer <mcrgcozer@gmail.com>
2022-05-06 12:49:04 -0300

    finished

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,42 @@ app.get('/users', async (req, res) => {
     })
     res.json(post)
   })
-
+  app.post(`/user`, async (req, res) => {
+    const result = await prisma.user.create({
+      data: { ...req.body },
+    })
+    res.json(result)
+  })
+  
+  app.post(`/post`, async (req, res) => {
+    const { title, content, authorEmail } = req.body
+    const result = await prisma.post.create({
+      data: {
+        title,
+        content,
+        published: false,
+        author: { connect: { email: authorEmail } },
+      },
+    })
+    res.json(result)
+  })
+  app.put('/post/publish/:id', async (req, res) => {
+    const { id } = req.params
+    const post = await prisma.post.update({
+      where: { id: Number(id) },
+      data: { published: true },
+    })
+    res.json(post)
+  })
+  
+  app.delete(`/post/:id`, async (req, res) => {
+    const { id } = req.params
+    const post = await prisma.post.delete({
+      where: { id: Number(id) },
+    })
+    res.json(post)
+  })
+  
 app.listen(3000, () =>
   console.log('REST API server ready at: http://localhost:3000'),
 )
